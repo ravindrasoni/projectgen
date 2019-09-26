ecf7aa5e2e5aa74662de93d251edfee8e61bab22
Ravindra Soni <soni@nickelfox.com>
2019-09-26 17:14:26 +0530

    fix: bugs

--- lib/projectgen/cli.rb
+++ lib/projectgen/cli.rb
@@ -5,7 +5,7 @@ module ProjectGen
   class CLI < Thor
 
 	desc "gen", "Generate project from template"
-	method_option :github_url, :required, aliases: "-g", :desc => "provide github_url with the mappings"
+	method_option :github_url, aliases: "-g", :desc => "provide github_url with the mappings", :required => true
 	def gen
 	    puts ProjectGen::Generator.generate_project(options[:github_url])
 	end
--- lib/projectgen/version.rb
+++ lib/projectgen/version.rb
@@ -1,3 +1,3 @@
 module ProjectGen
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
